53b28b827da36697363d8c8c2c9a7ef09c1b1d24
vinosri <vinods.scm@gmail.com>
2016-04-13 21:32:49 -0600

    Added + to visitor count functionality

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ visitor = 0
 get '/' do
   time = Time.now	
   visitor = += 1
-  'Hello world! The Currrent time is ' +  time.strftime('%H:%M') + ' GMT''.' ' You are Visitor Number ' visitor.to_s'.'
+  'Hello world! The Currrent time is ' +  time.strftime('%H:%M') + ' GMT''.' + ' You are Visitor Number ' visitor.to_s'.'
 end
