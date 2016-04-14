c111f9fd8a7afd9e35eb63714e65c1653426c19e
vinosri <vinods.scm@gmail.com>
2016-04-13 21:28:12 -0600

    Added Visitor Count functionality

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
+visitor = 0
 
 get '/' do
   time = Time.now	
-  'Hello world! The Currrent time is ' +  time.strftime('%H:%M') + ' GMT''.'
+  visitor = += 1
+  'Hello world! The Currrent time is ' +  time.strftime('%H:%M') + ' GMT''.' ' You are Visitor Number ' visitor.to_s'.'
 end
